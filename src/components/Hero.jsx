f94cb34ef851d9380433d0afa0ecfdc09c4f05f1
Tabuke Ezekiel <tabukeezekiel9@gmail.com>
2025-10-17 00:43:28 +0100

    Completed hero and responsive navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
       <h1 className="absolute z-20 p-4 text-4xl font-bold text-white -translate-x-1/2 text-nowrap sm:text-5xl -translate-y-3/4 top-1/2 left-1/2">
         <span className="text-amber-400">Hot,</span>{" "}
         <span className="text-green-500">fresh </span>pizzas 🍕 <br /> delivered{" "}
-        <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-6 before:bg-pink-500 mt-3">
-          <div class="relative text-white dark:text-gray-950 italic flex gap-1 items-end w-30 sm:w-44">
+        <span className="relative inline-block mt-3 before:absolute before:-inset-1 before:block before:-skew-y-6 before:bg-pink-500">
+          <div className="relative flex items-end gap-1 italic text-white dark:text-gray-950 w-30 sm:w-44">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width={`${window.innerWidth < 640 ? "36" : "48"}`}
               height="48"
               fill="currentColor"
-              class="bi bi-lightning-charge"
+              className="bi bi-lightning-charge"
               viewBox="0 0 16 16"
             >
               <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41z" />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../src/assets/Logo.png";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <nav>
-      <span className="flex bg-amber-500">
+    <nav className="flex items-center justify-between px-4 py-2 bg-white shadow sm:px-28 sm:py-0">
+      <span className="flex gap-3 cursor-pointer">
         <img src={logo} alt="Website's logo" />
-        <h4 className="text-2xl font-extrabold">DOMINO'S PIZZA</h4>
+        <h4 className="inline-block text-2xl font-extrabold text-transparent bg-gradient-to-r from-blue-700 via-purple-950 to-red-500 bg-clip-text">
+          DOMINO'S PIZZA
+        </h4>
       </span>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      {open && (
+        <ul className="nav-links animate-accordion-down gap-10 *:font-extrabold *:text-lg *:text-blue-600 *:italic sm:*:not-italic absolute top-16 sm:top-0 z-40 right-0 py-7 sm:relative flex-col sm:flex-row oswald flex bg-white w-1/2 h-[90vh] sm:h-1/12">
+          <Link to="/menu">
+            <li>MENU</li>
+          </Link>
+          <Link to="/deals">
+            <li>DEALS</li>
+          </Link>
+          <Link to="/about-us">
+            <li>ABOUT US</li>
+          </Link>
+          <Link to="/contact">
+            <li>CONTACT</li>
+          </Link>
+          <Link
+            to="/cart"
+            className="relative text-blue-900 sm:hidden outline-4 animate-pulse outline-red-700"
+          >
+            <li>CART</li>
+          </Link>
+        </ul>
+      )}
+      <Link className="relative hidden sm:flex" to="/cart">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={`${window.innerWidth < 450 ? "18" : "26"}`}
+          height={`${window.innerHeight < 450 ? "18" : "26"}`}
+          fill="blue"
+          className="relative font-bold cursor-pointer bi bi-cart3-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+        </svg>
+        <div className="absolute -top-1 -right-1 w-1.5 h-1.5 bg-red-500 rounded-full animate-ping"></div>
+      </Link>
+      <div
+        className="relative flex sm:hidden"
+        onClick={(e) => {
+          e.preventDefault();
+          setOpen(!open);
+          console.log(open);
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          fill="blue"
+          className="relative block cursor-pointer bi bi-list sm:hidden"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+          />
+        </svg>
+        <div className="absolute -top-1 -right-1 w-1.5 h-1.5 bg-red-500 rounded-full animate-ping"></div>
+      </div>
     </nav>
   );
 };
